d941e9aa861a8289026bf72584af6f36b81457fb
AliMortadaAnani <alimortadaanani@gmail.com>
2025-06-09 00:37:33 +0300

    setup

---
+++ GeneralFiles.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+string GeneralPath = "C:/Users/Ali/source/repos/Course_19/BankConsoleApp";
+string CurrenciesPath = GeneralPath + "/Currencies.txt";
+string ClientsPath = GeneralPath + "/Clients.txt";
+string UsersPath = GeneralPath + "/Users.txt";
+string TransactionsPath = GeneralPath + "/TransfersLog.txt";
+string LoginRegisterPath = GeneralPath + "/LoginRegister.txt";
