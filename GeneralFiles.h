ff2ca23057170f5c5abe78d4b397c69bb555ad0a
Ali Mortada Anani <alimortadaanani@gmail.com>
2026-01-23 08:26:01 +0200

    Fix file paths in GeneralFiles.h
    Updated file paths to be fully specified and Windows-compatible.

--- GeneralFiles.h
+++ GeneralFiles.h
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string GeneralPath = "C:/Users/Ali/source/repos/Course_19/BankConsoleApp";
-string CurrenciesPath = GeneralPath + "/Currencies.txt";
-string ClientsPath = GeneralPath + "/Clients.txt";
-string UsersPath = GeneralPath + "/Users.txt";
-string TransactionsPath = GeneralPath + "/TransfersLog.txt";
-string LoginRegisterPath = GeneralPath + "/LoginRegister.txt";
+
+// All paths are now fully specified and Windows-compatible
+string GeneralPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp";
+string CurrenciesPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp\\Currencies.txt";
+string ClientsPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp\\Clients.txt";
+string UsersPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp\\Users.txt";
+string TransactionsPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp\\TransfersLog.txt";
+string LoginRegisterPath = "C:\\Users\\Ali\\source\\repos\\Course_19\\BankConsoleApp\\LoginRegister.txt";
